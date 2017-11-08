a9b80b4f32ee537f25e3e6619732d5d2185ffc5e
Jens Gebhardt <dt0a35@canda.com>
2017-11-08 16:12:13 +0100

    feat: add new reducers

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import C from './constants'
-import { skiDay } from './store/reducers'
+import { allSkiDays } from './store/reducers'
 
-const state = null
+const state = [{
+    "resort" : "Kirkwood",
+    "date" : "2016-12-15",
+    "powder" : true,
+    "backcountry" : false
+
+}, {
+        "resort" : "Boreal",
+        "date" : "2016-12-16",
+        "powder" : false,
+        "backcountry" : false
+    }]
 
 const action = {
-    type: C.ADD_DAY,
-    payload: {
-        "resort": "Heavenly",
-        "date": "2016-12-16",
-        "powder": true,
-        "backcountry": false
-    }
+    type: C.REMOVE_DAY,
+    payload: "2016-12-15"
 }
 
-const nextState = skiDay(state, action)
+const nextState = allSkiDays(state, action)
 
 console.log(`
- initial state: ${state}
+ initial state: ${JSON.stringify(state)}
 
  action: ${JSON.stringify(action)}
 
